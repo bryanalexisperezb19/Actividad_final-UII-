c0dd4effe1d37c79edf509ecb1ee6cd3bbc6e6b1
bryanalexisperezb19 <91656436+bryanalexisperezb19@users.noreply.github.com>
2021-11-05 01:33:32 -0600

    Add files via upload

---
+++ empleado.php
@@ -0,0 +1,78 @@
+<html>
+<link rel="stylesheet" href="estilos.css">
+<meta charset="utf-8">
+<head> 
+    <title> Vivebus </title>
+</head>
+<body>
+    <div id="contenedor">
+        <div id="header">
+            <h1>Vivebus</h1>
+            <h2>Bryan Alexis Perez Bermudez 5-I</h2>
+            <div id="nav">
+               
+            
+                <ul class="menu">
+                    <li><a href="vivebus.php"> V i v e b u s </a></li>
+                    <li><a href="#"> J u s t i f i c a c i o n </a></li>
+                    <li><a href="#"> O b j e t i v o s </a></li>
+                    <li><a href="cliente.php"> C l i e n t e  </a></li>
+                    <li><a href="empleado.php"> E m p l e a d o </a></li>
+                </ul>
+
+            </div>
+        </div>
+        <div id="contenido">
+            <div id="section">
+                <h3>Nuestros empleados</h3> 
+                <p>A partir del pasado 19 de diciembre inició el periodo vacacional para una tercera parte de la plantilla de trabajadores al servicio del Gobierno del Estado</p>
+                      <br/>
+                      <div class="centrado">
+                        <img src="img/empleado.jpg" alt="Vivebus" >
+                      </div>
+                      <br/>
+                      <h6> 
+                 <li>Helio, La Cuesta</li>
+                      <li>  Aserraderos, Puerto Tarento </li>
+                      <li> Aeropuerto, Centro Zaragoza y Barbachano</li>
+                        Horario:
+                       <li> Todos los dias de la semana de las 4:30am - 10pm</li>
+
+                      </h6>
+                      <br/>
+                     <h4>Para cualquier informacion llama al:</h4>
+                     <h5>
+                      656-811-87-60
+                     </h5>
+            </div>
+            <div id="aside"> 
+                <article class="articulo">
+                    <h8>Vacantes</h8>
+                    <br/>
+                    <h7>Son adquiridas y recargadas en diversos autorizados, así como en las estaciones del propio sistema de transporte. Esta targeta tiene un costo de 30 pesos.</h7>
+                    <br/>
+                      <div class="centrado">
+                        <img src="img/empleado2.jpg" alt="Targeta" height="110px" width="110px" >
+                      </div>
+                     
+                </article>
+                <article class="articulo">
+                    <h8>horarios</h8>
+                    <br/>
+                    <h7>Estas poseen un 50% de decuento a la tarifa normal. Al igual que la targeta general tiene un costo de 30 pesos, y el pase de 4 pesos.</h7>
+                    <br/>
+                      <div class="centrado">
+                        <img src="img/empleado1.jpg" alt="Targeta" height="120px" width="120px" >
+                      </div>
+                      
+                </article>
+            </div>
+        </div>
+        <div id="footer">
+             <footer>
+                 Todos los Derechos Reservados | Vivebus | Bryan Bermudez Copiright |
+             </footer>
+        </div>
+    </div>
+</body>
+</html>
